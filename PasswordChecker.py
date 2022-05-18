e08c14ed50af2eb63b7ebc0aed9ff38863238bfc
Ender <the.crazycat@outlook.com>
2022-05-18 11:25:30 +0100

    Initial commit for experimental branch

--- PasswordChecker.py
+++ PasswordChecker.py
@@ -1,50 +1,71 @@
 import time
 import sys
 
+
 def passwordcheck(password):
     """
-    The passwordcheck function checks if the password entered by a user matches the real password.
-    If it does, then True is returned. If not, False is returned.
-    
-    :param password: Used to Store the password entered by the user.
-    :return: The result variable.
-    
-    :doc-author: Ender and Trelent
+    The passwordcheck function checks the password that is entered by the user and returns a permission level.
+    The function checks if the password matches any of three predefined passwords, returning a corresponding
+    permission level. If no match is found, it returns False.
+
+    :param password: Used to store the password entered by the user.
+    :return: The permissionlevel.
     """
-    
-    realpassword = "W3lc0met01h3h0M3L4nD"
-    if password == realpassword:
-        result = True
-        return result
+    global permissionlevel
+    permissionlevel = ""
+    passadmin = "W3lc0met01h3h0M3L4nD"
+    pass1 = "iloveb0afts"
+    pass2 = "bean0s13supreme"
+    if password == passadmin:
+        permissionlevel = 0
+        return permissionlevel
+    elif password == pass1:
+        permissionlevel = 1
+        return permissionlevel
+    elif password == pass2:
+        permissionlevel = 2
+        return permissionlevel
     else:
-        result = False
-        return result
-    
-passwordcheck(input ("Enter your password here"))
-
-global result
+        permissionlevel = False
+        return permissionlevel
 
 
-if result == True:
-    print ("Password is correct!")
+passwordcheck(input("Enter your password here"))
+
+
+if permissionlevel == 0:
+    print("Password is correct!")
     time.sleep(2)
-    username = input ("What is your name?")
+    username = input("What is your name?")
     time.sleep(0.6)
-    print ("Hello," + username)
+    print("Hello, Administrator " + username)
     time.sleep(0.6)
-    print ("Acessing terminal...")
+    print("Acessing terminal...")
     import Terminal.py
-elif result != True:
-    print ("Error, your password is incorrect.")
+elif permissionlevel == 1:
+    print("Password is correct!")
+    time.sleep(2)
+    print("Welcome, RaftLover456")
     time.sleep(0.6)
-    print("Please restart the application and try again.")
+    print("Acessing terminal...")
+    import Terminal.py
+elif permissionlevel == 2:
+    print("Password is correct!")
+    time.sleep(2)
+    print("Welcome, beanosdude")
+    time.sleep(0.6)
+    print("Acessing terminal...")
+    import Terminal.py
+elif permissionlevel == False:
+    print("Password is incorrect!")
+    print("Please restart the program and try again.")
     sys.exit()
+
 else:
-    print ("If you've got to this screen, you probably messed with the code or you input a value to break the code branch")
+    print(
+        "If you've got to this screen, you probably messed with the code or you input a value to break the code branch"
+    )
     time.sleep(0.6)
-    print ("Either way, you're going to have to fix the code manually (or if you just entered a wrong value, restart the program.)")
-    
-
-
-
-
+    print(
+        "Either way, you're going to have to fix the code manually (or if you just entered a wrong value, restart the program.)"
+    )
